4d853e1bf87c3f47c7734ee4835c46b448461d9d
prush08 <parkerrushton@gmail.com>
2015-04-16 11:17:54 -0600

    Initial Commit

---
+++ My Rep (Rain)/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  My Rep (Rain)
+//
+//  Created by Parker Rushton on 4/16/15.
+//  Copyright (c) 2015 PJayRushton. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
