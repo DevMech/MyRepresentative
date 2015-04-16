9742d88b2198b2a83ddfc10258a6b398286ddff4
prush08 <parkerrushton@gmail.com>
2015-04-16 13:20:46 -0600

    Adds Custom HomeCollectionViewCell

---
+++ My Rep (Rain)/HomeCollectionViewCell.h
@@ -0,0 +1,13 @@
+//
+//  HomeCollectionViewCell.h
+//  My Rep (Rain)
+//
+//  Created by Parker Rushton on 4/16/15.
+//  Copyright (c) 2015 PJayRushton. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HomeCollectionViewCell : UICollectionViewCell
+
+@end
