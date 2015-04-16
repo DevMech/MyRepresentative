7ae4f7243104ce7a522ed5b3bcc1f3e479ff12e6
prush08 <parkerrushton@gmail.com>
2015-04-16 16:08:39 -0600

    Adds Appearance Controller

---
+++ My Rep (Rain)/AppearanceController.h
@@ -0,0 +1,14 @@
+//
+//  AppearanceController.h
+//  My Rep (Rain)
+//
+//  Created by Parker Rushton on 4/16/15.
+//  Copyright (c) 2015 PJayRushton. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface AppearanceController : NSObject
+
++(void)setupAppearance;
+@end
---
+++ My Rep (Rain)/AppearanceController.m
@@ -0,0 +1,16 @@
+//
+//  AppearanceController.m
+//  My Rep (Rain)
+//
+//  Created by Parker Rushton on 4/16/15.
+//  Copyright (c) 2015 PJayRushton. All rights reserved.
+//
+
+#import "AppearanceController.h"
+
+@implementation AppearanceController
+
++(void)setupAppearance {
+    
+}
+@end
