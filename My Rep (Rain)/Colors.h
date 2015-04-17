28cdea50a526e7c9b198104c2e6295d638f708ee
prush08 <parkerrushton@gmail.com>
2015-04-17 13:38:55 -0600

    Adds Static Colors header file

---
+++ My Rep (Rain)/Colors.h
@@ -0,0 +1,16 @@
+//
+//  Colors.h
+//  My Rep (Rain)
+//
+//  Created by Parker Rushton on 4/17/15.
+//  Copyright (c) 2015 PJayRushton. All rights reserved.
+//
+
+#ifndef My_Rep__Rain__Colors_h
+#define My_Rep__Rain__Colors_h
+
+#define RepublicanRed [UIColor colorWithRed:0.816 green:0.00784 blue:0.106 alpha:1]
+#define DemocratBlue [UIColor colorWithRed:0.051 green:0.129 blue:0.631 alpha:1]
+#define LibYellow [UIColor colorWithRed:0.988 green:0.847 blue:0 alpha:1]
+
+#endif
