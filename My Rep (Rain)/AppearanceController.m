569e6bb14e9e810e294ae48534b65224a752f3a2
prush08 <parkerrushton@gmail.com>
2015-04-17 13:43:30 -0600

    Adds appearance controller and stylizes navbar

--- My Rep (Rain)/AppearanceController.m
+++ My Rep (Rain)/AppearanceController.m
@@ -11,6 +11,18 @@
 @implementation AppearanceController
 
 +(void)setupAppearance {
+    //Navigation Bars
+    [[UIApplication sharedApplication] setStatusBarStyle:UIStatusBarStyleLightContent];
     
+    [[UINavigationBar appearance] setBarStyle:UIBarStyleBlack];
+    [[UINavigationBar appearance] setBarTintColor:[UIColor blueColor]];
+    [[UINavigationBar appearance] setTintColor:[UIColor whiteColor]];
+    [[UINavigationBar appearance] setOpaque:YES];
+    [[UINavigationBar appearance] setTranslucent:NO];
+    
+    [[UINavigationBar appearance] setTitleTextAttributes: [NSDictionary dictionaryWithObjectsAndKeys:[UIFont fontWithName:@"AppleSDGothicNeo-Bold" size:24], NSFontAttributeName, nil]];
+    
+    //Navigation Bar Items
+    [[UIBarButtonItem appearanceWhenContainedIn:[UINavigationBar class], nil] setTitleTextAttributes:@{NSFontAttributeName:[UIFont fontWithName:@"AppleSDGothicNeo-Medium" size:16]} forState:UIControlStateNormal];
 }
 @end
