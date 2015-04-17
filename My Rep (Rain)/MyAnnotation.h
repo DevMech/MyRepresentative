1124fe19445f4e945dce15fef162c59962d9f211
prush08 <parkerrushton@gmail.com>
2015-04-17 13:38:44 -0600

    Adds MyAnnotation (Subclass of MKAnnotation)

---
+++ My Rep (Rain)/MyAnnotation.h
@@ -0,0 +1,16 @@
+//
+//  MyAnnotation.h
+//  My Rep (Rain)
+//
+//  Created by Parker Rushton on 4/16/15.
+//  Copyright (c) 2015 PJayRushton. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <MapKit/MapKit.h>
+
+@interface MyAnnotation : NSObject <MKAnnotation>
+@property (nonatomic, assign) CLLocationCoordinate2D coordinate;
+@property (nonatomic, copy) NSString *title;
+@property (nonatomic, copy) NSString *subtitle;
+@end
---
+++ My Rep (Rain)/MyAnnotation.m
@@ -0,0 +1,13 @@
+//
+//  MyAnnotation.m
+//  My Rep (Rain)
+//
+//  Created by Parker Rushton on 4/16/15.
+//  Copyright (c) 2015 PJayRushton. All rights reserved.
+//
+
+#import "MyAnnotation.h"
+
+@implementation MyAnnotation
+
+@end
