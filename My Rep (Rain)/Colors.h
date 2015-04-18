03ca8940154b9872709cd7f23719e0718fb8a2cb
prush08 <parkerrushton@gmail.com>
2015-04-17 20:49:55 -0600

    Adds Hex comments for clarity

--- My Rep (Rain)/Colors.h
+++ My Rep (Rain)/Colors.h
@@ -10,7 +10,10 @@
 #define My_Rep__Rain__Colors_h
 
 #define RepublicanRed [UIColor colorWithRed:0.816 green:0.00784 blue:0.106 alpha:1]
+//D0021B Hex
 #define DemocratBlue [UIColor colorWithRed:0.051 green:0.129 blue:0.631 alpha:1]
+//0D21A1 Hex
 #define LibYellow [UIColor colorWithRed:0.988 green:0.847 blue:0 alpha:1]
+//FCD800 Hex
 
 #endif
