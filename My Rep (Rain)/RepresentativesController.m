0ba81d4a0e0d4229948401c6672eca57773e40a6
prush08 <parkerrushton@gmail.com>
2015-04-17 20:52:47 -0600

    Adds API hit to senators

--- My Rep (Rain)/RepresentativesController.m
+++ My Rep (Rain)/RepresentativesController.m
@@ -2,26 +2,31 @@
 //  RepresentativesController.m
 //  My Rep (Rain)
 //
-//  Created by Parker Rushton on 4/7/15.
+//  Created by Parker Rushton on 4/16/15.
 //  Copyright (c) 2015 PJayRushton. All rights reserved.
 //
 
 #import "RepresentativesController.h"
 #import "Representative.h"
 
+@interface RepresentativesController ()
+
+@property (nonatomic) NSMutableArray *senatorsArray;
+
+@end
+
 @implementation RepresentativesController
 
-+ (RepresentativesController *)sharedInstance {
++(RepresentativesController *)sharedInstance {
     static RepresentativesController *sharedInstance = nil;
     static dispatch_once_t onceToken;
     dispatch_once(&onceToken, ^{
         sharedInstance = [RepresentativesController new];
-        
     });
     return sharedInstance;
 }
 
-- (void)searchRepWithInfo:(NSString *)info searchType:(NSInteger)searchType completion:(void (^)(BOOL success))completion {
+-(void)searchRepWithInfo:(NSString *)info searchType:(NSInteger)searchType completion:(void (^)(BOOL success))completion {
     NSURL *url;
     switch (searchType) {
         case 0:
@@ -29,9 +34,34 @@
             break;
         case 1:
             url = [NSURL URLWithString:[NSString stringWithFormat:@"http://whoismyrepresentative.com/getall_reps_bystate.php?state=%@&output=json", info]];
+            [self searchSenatorsWithInfo:info andType:searchType];
             break;
         case 2:
             url = [NSURL URLWithString:[NSString stringWithFormat:@"http://whoismyrepresentative.com/getall_reps_byname.php?name=%@&output=json", info]];
+            [self searchSenatorsWithInfo:info andType:searchType];
+            break;
+    }
+    NSError *error;
+    NSArray *array = [[NSJSONSerialization JSONObjectWithData:[NSData dataWithContentsOfURL:url] options:NSJSONReadingAllowFragments error:&error] objectForKey:@"results"];
+    
+    NSMutableArray *tempRepsList = [NSMutableArray new];
+    
+    for (NSDictionary *dictionary in array) {
+        Representative *rep = [[Representative alloc] initWithDictionary:dictionary];
+        [tempRepsList addObject:rep];
+    }
+        self.repsArray = [tempRepsList arrayByAddingObjectsFromArray:self.senatorsArray];
+        completion(YES);
+}
+
+-(void)searchSenatorsWithInfo:(NSString *)info andType:(NSInteger)searchType{
+    NSURL *url;
+    switch (searchType) {
+        case 1:
+            url = [NSURL URLWithString:[NSString stringWithFormat:@"http://whoismyrepresentative.com/getall_sens_bystate.php?state=%@&output=json", info]];
+            break;
+        case 2:
+            url = [NSURL URLWithString:[NSString stringWithFormat:@"http://whoismyrepresentative.com/getall_sens_byname.php?name=%@&output=json", info]];
             break;
     }
     NSError *error;
@@ -44,9 +74,9 @@
         [tempRepsList addObject:rep];
     }
     if (array) {
-        self.repsArray = tempRepsList;
-        completion(YES);
+        self.senatorsArray = tempRepsList;
     }
+
 }
 
 @end
